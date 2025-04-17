ae345cd739cecb5dcaa91424789844f683f2a0c5
FortunateC0der <fortunate.mothoana@gmail.com>
2025-04-17 15:01:22 -0700

    update components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM(document.getElementById("root")).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-    <h1>Hello, Vite + React + Bootstrap!</h1>
-  </StrictMode>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
