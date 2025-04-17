f7fb4fc2a64eac4a9a1636bf84668d38ff285c4b
FortunateC0der <fortunate.mothoana@gmail.com>
2025-04-17 02:47:38 -0700

    bootstrap

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+
+ReactDOM(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    <h1>Hello, Vite + React + Bootstrap!</h1>
+  </StrictMode>
+);
