7b93e4e9a14bcb2d8ae4b8f198916a57be7165db
FortunateC0der <fortunate.mothoana@gmail.com>
2025-04-17 14:40:51 -0700

    app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import logo from "./logo.svg";
 import "./App.css";
+import { NavBar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
